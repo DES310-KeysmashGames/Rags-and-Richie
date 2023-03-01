b861359757dba7a69448d8e1262c374019703343
Leslie <47124029+Geezertron@users.noreply.github.com>
2023-03-01 16:08:54 +0000

    items showing
    list of "scavenged" items shows in buttons

--- Assets/Scripts/InventorySelection.cs
+++ Assets/Scripts/InventorySelection.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventorySelection : MonoBehaviour
 {
@@ -11,4 +12,23 @@ public class InventorySelection : MonoBehaviour
     [SerializeField] public List<BaseItem> scavengedItems = new List<BaseItem>();
     //list for items to be selected into
     [SerializeField] public List<BaseItem> chosenInventory = new List<BaseItem>();
+    [SerializeField] private Image[] scavengedItemSprites;
+    [SerializeField] private Image[] sprites;
+
+    private void Start(){
+        AssignSprites();
+    }
+
+    public Sprite GetSprite(int i)
+    {
+        //return CurrentItems[itemNo].frontSprite;
+        return scavengedItems[i].frontSprite;
+    }
+    private void AssignSprites(){
+        for (int i = 0; i < scavengedItemSprites.Length; ++i)
+        {
+            scavengedItemSprites[i].enabled = true;
+            scavengedItemSprites[i].sprite = GetSprite(i);
+        }
+    }
 }
