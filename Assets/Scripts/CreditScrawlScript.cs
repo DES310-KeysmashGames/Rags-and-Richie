db328d7e41d11d19ba6d3853abc5913c08e5382c
Calvin Cheung <78021248+Xatuso@users.noreply.github.com>
2023-05-10 16:01:31 +0100

    Update
    Changed timers for credit sequence.

--- Assets/Scripts/CreditScrawlScript.cs
+++ Assets/Scripts/CreditScrawlScript.cs
@@ -12,14 +12,14 @@ public class CreditScrawlScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timer = 19.0f;
+        timer = 30.0f;
         creditEvent.Post(gameObject);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        credits.transform.position = new Vector3(credits.transform.position.x, (float)((double)credits.transform.position.y + 0.5), credits.transform.position.z);
+        credits.transform.position = new Vector3(credits.transform.position.x, (float)((double)credits.transform.position.y + 2.0f), credits.transform.position.z);
         timer -= Time.deltaTime;
         if (timer <= 0)
         {
