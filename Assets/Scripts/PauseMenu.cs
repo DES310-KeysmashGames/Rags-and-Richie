2012029d64f38944418e7fbf7bcf82700ce0ab32
Joel <78077198+SzionX@users.noreply.github.com>
2023-07-04 17:25:11 +0100

    Basic pause menu finished and implemented

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -3,11 +3,46 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool gamePaused = false;
     public GameObject pauseMenu;
+
+    [Header("Menu Buttons")]
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button settingsButton;
+    [SerializeField] private Button menuButton;
+
+    private void Awake()
+    {
+        //Resume Game via button click
+        resumeButton.onClick.AddListener(() =>
+        {
+            //Play button sound
+
+            ResumeGame();
+        });
+
+        //Bring up settings/options menu
+        settingsButton.onClick.AddListener(() =>
+        {
+            //Play button sound
+
+            Settings();
+        });
+
+        //Reset Time, unpause game, return to main menu
+        menuButton.onClick.AddListener(() =>
+        {
+            Time.timeScale = 1f;
+            gamePaused = false;
+            Loader.Load(Loader.Scene.MainMenuScene);
+
+            //Potentially pause background music
+        });
+    }
 
     // Update is called once per frame
     void Update()
@@ -24,17 +59,26 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    private void PauseGame()
+    //Pause time, trigger Singleton, activate Menu UI
+    void PauseGame()
     {
-        pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         gamePaused = true;
+        pauseMenu.gameObject.SetActive(true);
     }
 
-    private void ResumeGame()
+    //Reset time, 
+    void ResumeGame()
     {
-        pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         gamePaused = false;
+        pauseMenu.gameObject.SetActive(false);
+    }
+
+    void Settings()
+    {
+        Debug.Log("Imagine that theres a full working settings menu here...");
+
+        //Various button sounds
     }
 }
